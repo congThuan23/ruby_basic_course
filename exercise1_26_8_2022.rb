7e4b80eb3f7be560bf9e96c666246fd33256dd15
Thuan Nguyen <congthuan701@gmail.com>
2022-08-27 17:46:45 +0700

    exercise 1 - week 1

---
+++ exercise1_26_8_2022.rb
@@ -0,0 +1,10 @@
+puts "Xin chào! Nguyễn Công Thuận 1911505310265 \n\n"
+
+print "Nhập số bất kì: "
+a = gets
+puts "Số vừa nhập: #{a}\n"
+
+print "Nhập vào 2 số: "
+b,c = gets.split
+puts "Số lớn nhất trong 2 số vừa nhập là: #{(b>c) ? b:c}"
+
